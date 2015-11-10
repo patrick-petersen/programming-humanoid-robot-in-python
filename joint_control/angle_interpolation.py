252c2f3097334d85ccca2ebbb19920c8f127c16c
patrick-petersen <petersen.p@gmx.net>
2015-11-10 14:44:31 +0100

    improved algorithm, to interpolate between the times of the two wrapping (prev. and next) keyframes

--- joint_control/angle_interpolation.py
+++ joint_control/angle_interpolation.py
@@ -33,7 +33,6 @@ class AngleInterpolationAgent(PIDAgent):
                  sync_mode=True):
         super(AngleInterpolationAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.keyframes = ([], [], [])
-	
     
     def think(self, perception):
         target_joints = self.angle_interpolation(self.keyframes, perception)
@@ -42,18 +41,18 @@ class AngleInterpolationAgent(PIDAgent):
 
     def angle_interpolation(self, keyframes, perception):
         target_joints = {}
-	global firstTime
-	global timeSet
+        global firstTime
+        global timeSet
         # YOUR CODE HERE
         self.currentTime = self.perception.time
         if timeSet == False:
             firstTime = self.currentTime
-  	    timeSet = True	    
+            timeSet = True
         (names, times, keys) = keyframes
 
         timeInKeyframes = self.currentTime - firstTime
 
-	maxTimesForNames = 0
+        maxTimesForNames = 0
         for nameIndex in range(0, len(names)):
             #iterare through all joints
             name = names[nameIndex]
@@ -64,28 +63,32 @@ class AngleInterpolationAgent(PIDAgent):
             #nextTime = timesForName[0]
             i = 0
             while i < len(timesForName)-2 and timeInKeyframes > timesForName[i]:
-		i += 1
-              
-	    P0 = (timesForName[i], keysForName[i][0])
- 	    P1 = (timesForName[i] + keysForName[i][2][1], keysForName[i][0] + keysForName[i][2][2])
+                i += 1
+
+            P0 = (timesForName[i], keysForName[i][0])
+            P1 = (timesForName[i] + keysForName[i][2][1], keysForName[i][0] + keysForName[i][2][2])
             #P2 ist out of index range for some reason when len(timeForName)-1
-	    P2 = (timesForName[i+1], keysForName[i+1][0]) #
+            P2 = (timesForName[i+1], keysForName[i+1][0]) #
             P3 = (timesForName[i+1] + keysForName[i+1][1][1], keysForName[i+1][0] + keysForName[i+1][1][2]) 
-	    t = timeInKeyframes	    
-	    k = timesForName[len(timesForName)-1]
-	    # normally the right time intervall border, but time is in this case not element of [0,1]
-	    # k should be the last Keyframe, i think
-	    target_joints[name] = (k-t) ** 3 * P0[1] + 3*(k-t) ** 2 * t * P1[1] + 3 * (k-t) * t ** 2 * P2[1] + t**3 * P3[1]
-	    if timesForName[len(timesForName)-1]> maxTimesForNames:
-		maxTimesForNames = timesForName[len(timesForName)-1]
-	# also, don't we just need on of the coordinates, because the second would be the time, wich we have
-	#1-t mit 1 = k wie oben
+            t = timeInKeyframes - timesForName[i]
+            k = timesForName[i+1] - timesForName[i]
+            # normally the right time intervall border, but time is in this case not element of [0,1]
+            # k should be the next Keyframe, relaive to the prev. keyframe
+            target_joints[name] = ((k-t) ** 3) * P0[1] + 3 * ((k-t) ** 2) * t * P1[1] + 3 * (k-t) * (t ** 2) * P2[1] + (t**3) * P3[1]
+            if timesForName[len(timesForName)-1]> maxTimesForNames:
+                maxTimesForNames = timesForName[len(timesForName)-1]
+
+
+            print(name, timesForName[i], timeInKeyframes, timesForName[i+1])
+            
+        # also, don't we just need on of the coordinates, because the second would be the time, wich we have
+        #1-t mit 1 = k wie oben
         #(1-t) ** 3 * P0 + 3*(1-t) ** 2 * t * P1 + 3 * (1-t) * t ** 2 * P2 + t**3 * P3
-	
-	if timeInKeyframes > maxTimesForNames:
-	    timeSet = False
-	print target_joints
- 	return target_joints
+        
+        if timeInKeyframes > maxTimesForNames:
+            timeSet = False
+
+        return target_joints
 
 if __name__ == '__main__':
     agent = AngleInterpolationAgent()
