1ee1c8465ab560354120086fda6b9a10f11f0ddd
patrick-petersen <petersen.p@gmx.net>
2015-11-12 15:37:49 +0100

    learned posture (with 100% of the data) and got joint angle order in recognize_posture() from someone else

--- joint_control/recognize_posture.py
+++ joint_control/recognize_posture.py
@@ -13,6 +13,11 @@
 from angle_interpolation import AngleInterpolationAgent
 from keyframes import hello
 
+import pickle
+from os import listdir, path
+import numpy as np
+from sklearn import svm, metrics
+
 
 class PostureRecognitionAgent(AngleInterpolationAgent):
     def __init__(self, simspark_ip='localhost',
@@ -22,7 +27,10 @@ class PostureRecognitionAgent(AngleInterpolationAgent):
                  sync_mode=True):
         super(PostureRecognitionAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.posture = 'unknown'
-        self.posture_classifier = None  # LOAD YOUR CLASSIFIER
+        ROBOT_POSE_CLF = 'robot_pose.pkl'
+        self.posture_classifier = pickle.load(open(ROBOT_POSE_CLF))  # LOAD YOUR CLASSIFIER
+        ROBOT_POSE_DATA_DIR = 'robot_pose_data'
+        self.classes = listdir(ROBOT_POSE_DATA_DIR)
 
     def think(self, perception):
         self.posture = self.recognize_posture(perception)
@@ -31,6 +39,25 @@ class PostureRecognitionAgent(AngleInterpolationAgent):
     def recognize_posture(self, perception):
         posture = 'unknown'
         # YOUR CODE HERE
+
+        # YOUR CODE HERE
+        # get angles of: ['AngleX', 'AngleY', 'LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch']
+        angles = []
+        angles.append(perception.imu[0])
+        angles.append(perception.imu[1])
+        angles.append(perception.joint['LHipYawPitch'])
+        angles.append(perception.joint['LHipRoll'])
+        angles.append(perception.joint['LHipPitch'])
+        angles.append(perception.joint['LKneePitch'])
+        angles.append(perception.joint['RHipYawPitch'])
+        angles.append(perception.joint['RHipRoll'])
+        angles.append(perception.joint['RHipPitch'])
+        angles.append(perception.joint['RKneePitch'])
+        
+        angles = np.array(angles).reshape(1, -1)
+        
+        posture = self.posture_classifier.predict(angles)[0]
+        print self.classes[posture]
 
         return posture
 
